7b925f161f055278739c9a52850781801e9014dc
anupamdebnath6335 <anupamdebnath6335@gmail.com>
2022-12-12 02:58:02 +0530

    complete

--- gpt3_jsm/src/containers/header/Header.jsx
+++ gpt3_jsm/src/containers/header/Header.jsx
@@ -4,6 +4,7 @@ import ai from '../../assets/ai.png';
 import './header.css';
 
 const Header = () => (
+
   <div className="gpt3__header section__padding" id="home">
     <div className="gpt3__header-content">
       <h1 className="gradient__text">Let&apos;s Build Something amazing with GPT-3 OpenAI</h1>
